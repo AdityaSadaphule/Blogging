a9d6df3f2f7381324d5af3076e4b42d40c286190
Aditya Sadaphule <aditya.sadaphule.1999@gmail.com>
2022-09-17 23:25:53 +0530

    Initial commit

---
+++ src/main/java/co/mushu/blogging/BloggingSystemApplication.java
@@ -0,0 +1,13 @@
+package co.mushu.blogging;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloggingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/mushu/blogging/BloggingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package co.mushu.blogging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
