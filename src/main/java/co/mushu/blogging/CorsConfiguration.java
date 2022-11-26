8c00f316f21848f01797c4ecd6d111e87d6db09e
Aditya Sadaphule <aditya.sadaphule.1999@gmail.com>
2022-11-27 00:22:20 +0530

    Totally Removed CORS AND CSRF As the Blog creation API was not available

--- src/main/java/co/mushu/blogging/CorsConfiguration.java
+++ src/main/java/co/mushu/blogging/CorsConfiguration.java
@@ -1,9 +1,17 @@
 package co.mushu.blogging;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfiguration implements WebMvcConfigurer {
+
+    static{
+        System.out.println("Inside the Cors Configuration");
+    }
+
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/*")
